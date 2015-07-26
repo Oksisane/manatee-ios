d8b06149dc4988d24c0b1ed61d0a229b3bc72d24
Oksisane <ehsanmasdar@gmail.com>
2015-07-27 01:22:20 +0200

    Update storyboard, finish single cell first design pass with live data

--- ios/CourseCell.swift
+++ ios/CourseCell.swift
@@ -13,6 +13,7 @@ import YapDatabase
 class CourseCell:UITableViewCell,UITableViewDataSource,UITableViewDelegate{
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var gradeTable: UITableView!
+    var clearTable:Bool = false
     let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
     override func layoutSubviews() {
         super.layoutSubviews()
@@ -22,17 +23,27 @@ class CourseCell:UITableViewCell,UITableViewDataSource,UITableViewDelegate{
         self.gradeTable.delegate = self
         self.gradeTable.dataSource = self
     }
+    override func prepareForReuse() {
+        titleLabel.text = ""
+        clearTable = true
+        gradeTable.reloadData()
+        clearTable = false
+        gradeTable.reloadData()
+    }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell:SemesterCell = self.gradeTable.dequeueReusableCellWithIdentifier("SemesterCell") as! SemesterCell
-        println("making table....")
         for course in courses{
             if (titleLabel.text == course.title){
+                println("Loading " + course.title + " with values " + course.semseters[indexPath.row].cycles[0].average.grade.description)
                 cell.GradeOne.text = Int(course.semseters[indexPath.row].cycles[0].average.grade).description
             }
         }
         return cell
     }
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if (clearTable){
+            return 0
+        }
         return 2
     }
     
